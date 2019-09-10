27138a69a88a27dca74ed021796a3aaab3975b37
Lixquid <Lixquid@users.noreply.github.com>
2019-09-10 19:25:27 +0100

    Added layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
+
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
+    <div id="app" class="container my-5">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="line"></div>
+            </div>
+            <div class="col-auto">
+                <router-link to="/">
+                    <img src="@/assets/logo.png" alt="Lixquid" />
+                </router-link>
+            </div>
+            <div class="col">
+                <div class="line"></div>
+            </div>
         </div>
+        <nav class="nav">
+            <router-link to="/">Home</router-link>
+            <span class="separator">/</span>
+            <router-link to="/">Home</router-link>
+        </nav>
         <router-view />
     </div>
 </template>
 
-<style lang="stylus">
-@import "~bootstrap/dist/css/bootstrap.css"
-@import "~bootstrap-vue/dist/bootstrap-vue.css"
+<style lang="stylus" scoped>
+.nav
+    display block
+    margin-bottom 1em
+
+    color #bbb
+    text-align center
+    font-size 1.3em
+
+.separator
+    display inline-block
+    margin 0 .5em
+
+.line
+    width 60%
+    height 1px
+    margin 0 20%
+
+    background #ccc
 </style>
+
+<style lang="stylus" src="./App.styl"></style>

c9a2464bce4742e2467795e9605394a313dbae62
Lixquid <Lixquid@users.noreply.github.com>
2019-09-10 20:14:46 +0100

    Added interim Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        <h1>Hello!</h1>
+        <h1>Say Hello!</h1>
+        <ul class="lead">
+            <li>
+                <a href="https://github.com/lixquid">
+                    <i class="fab fa-github"></i>
+                    GitHub
+                </a>
+            </li>
+            <li>
+                <a href="https://gitlab.com/lixquid">
+                    <i class="fab fa-gitlab"></i>
+                    GitLab
+                </a>
+            </li>
+        </ul>
     </div>
 </template>

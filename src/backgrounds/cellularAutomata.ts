cae3c70112b3eda3c2d0e6b2a4a3cf396f77ae58
Lixquid <lixquid@lixquid.com>
2024-06-11 00:36:53 +0100

    Add different cellular automata to background

--- src/backgrounds/cellularAutomata.ts
+++ src/backgrounds/cellularAutomata.ts
@@ -1,9 +1,27 @@
 // Render a grid of cells that simulates various Cellular Automata rules.
 
+const automata = {
+	"Game of Life": [[2, 3], [3], "#0bab00"],
+	Assimilation: [[4, 5, 6, 7], [3, 4, 5], "#2200ff"],
+	"2x2": [[1, 2, 5], [3, 6], "#cccc00"],
+	Amoeba: [[1, 3, 5, 8], [3, 5, 7], "#cb00cb"],
+	Diamoeba: [[5, 6, 7, 8], [3, 5, 6, 7, 8], "#9b0000"],
+	Stains: [[2, 3, 5, 6, 7, 8], [3, 6, 7, 8], "#7100ff"],
+	Maze: [[1, 2, 3, 4, 5], [3], "#a7e39f"],
+	Coagulations: [[2, 3, 5, 6, 7, 8], [3, 7, 8], "#9acd32"],
+	"Walled Cities": [[2, 3, 4, 5], [4, 5, 6, 7, 8], "#0065f6"],
+} satisfies Record<string, [number[], number[], string]>;
+
 const cellSize = 10;
 const fps = 2.5;
-const surviveNeighbors = [2, 3];
-const reproduceNeighbors = [3];
+
+const variant = Object.keys(automata)[
+	Math.floor(Math.random() * Object.keys(automata).length)
+] as keyof typeof automata;
+
+const surviveNeighbors = automata[variant][0];
+const reproduceNeighbors = automata[variant][1];
+const variantColor = automata[variant][2];
 
 export function cellularAutomata(
 	ctx: CanvasRenderingContext2D,
@@ -32,7 +50,7 @@ export function cellularAutomata(
 		lastTime = Date.now();
 
 		// Render the cells
-		ctx.fillStyle = "#36f";
+		ctx.fillStyle = variantColor;
 		for (let y = 0; y < cellCountY; y++) {
 			for (let x = 0; x < cellCountX; x++) {
 				if (cells[y]![x]) {
@@ -71,6 +89,12 @@ export function cellularAutomata(
 			}
 		}
 		cells = newCells;
+
+		// Draw the name of the variant at the top of the canvas
+		ctx.fillStyle = "#fff";
+		ctx.font = "12px monospace";
+		ctx.textAlign = "center";
+		ctx.fillText(variant, canvas.width / 2, 12);
 
 		if (!stopSignal.stop) {
 			requestAnimationFrame(render);
